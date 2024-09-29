ab4be2e6d251f3442b0537739223bf9adad03164
ThomHags611 <thomashagos611@gmail.com>
2024-09-28 18:34:22 -0700

    switched from pre-built method to creating own calculation

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const display = document.getElementById("display");
+let input;
+let firstNum = 2;
+let secondNum = 2;
+console.log(firstNum + secondNum);
 
 function appendToDisplay(input)
 {
@@ -10,6 +14,37 @@ function clearDisplay(){
 }
 
 function calculate(){
+        secondNum = parseFloat(display.value);
+
+        console.log(firstNum + secondNum);
+        try{
+            switch(input)
+            {
+                case '+':
+                    display.value = (firstNum + secondNum);
+                    break;
+                case '-':
+                    display.value = firstNum - secondNum;
+                    break;
+                case '*':
+                    display.value = firstNum * secondNum;
+                    break;
+                case '/':
+                    display.value = firstNum / secondNum;
+                    break;
+                case '%':
+                    display.value = firstNum % secondNum;
+                default:
+                    break;
+            }
+        }catch(error){
+            display.value() = "error";
+
+        }
+            
+        
+        
+    
     try{
        display.value = eval(display.value); 
     }
@@ -20,4 +55,16 @@ function calculate(){
 
 function negate(){
     display.value *= -1;
+}
+
+function callAdd(){compute('+');}
+function callSub(){compute('-');}
+function callMul(){compute('*');}
+function callDiv(){compute('/');}
+function callMod(){compute('%');}
+
+function compute(input2){
+    firstNum = parseFloat(display.value);
+    input = input2;
+    display.value = "";
 }
